0c9e940d8aa5beed304f834fe57c017f36ab866e
ellettie <mi.5270gh@gmail.com>
2025-06-26 06:08:12 +0900

    スピナー機能を追加し、起動時にバナーを表示するように変更

--- bot/myutils.py
+++ bot/myutils.py
@@ -1,5 +1,11 @@
 import discord
+import google.genai
+import PIL
 from .config import Config
+import os
+import sys
+import time
+import logging
 
 def split_message(text, max_length=1900):
     if len(text) <= max_length:
@@ -42,3 +48,33 @@ def get_error_embed(description: str) -> discord.Embed:
         title=Config.EMBED_SET["error"]["title"], 
         description=description, 
         colour=Config.EMBED_SET["error"]["colour"])
+    
+def rgb(r, g, b): return f"\x1b[38;2;{r};{g};{b}m"  # 前景
+LOGO = rgb(114, 137, 218)
+GREEN  = rgb(80, 255, 180)
+GREY   = rgb(180, 180, 180)
+RESET  = "\x1b[0m"
+
+def print_banner(bot, start_ts: float) -> None:
+    print(f"{LOGO}{Config.LOGO}")
+
+    rows = [
+        (f"{LOGO}version{RESET}",     Config.VERSION),
+        (f"{LOGO}repo{RESET}",     Config.REPO_URL),
+        (f"{LOGO}developer{RESET}",     Config.AUTHOR),
+    ]
+    for key, val in rows:
+        print(f" {key:<10} : {GREY}{val}{RESET}")
+    print()
+    rows = [
+        (f"{GREEN}python{RESET}",   f"{sys.version_info.major}.{sys.version_info.minor}"),
+        (f"{GREEN}discord.py{RESET}", discord.__version__),
+        (f"{GREEN}google-genai{RESET}", google.genai.__version__),
+        (f"{GREEN}pillow{RESET}", PIL.__version__),
+        (f"{GREEN}user{RESET}",     f"{bot.user} ({bot.user.id})"),          # type: ignore
+        (f"{GREEN}guild{RESET}",   bot.guild_id),
+        (f"{GREEN}logLevel{RESET}", logging.getLevelName(logging.getLogger().level)),
+        (f"{GREEN}startup{RESET}",  f"{time.perf_counter() - start_ts:.2f}s"),
+    ]
+    for key, val in rows:
+        print(f" {key:<10} : {GREY}{val}{RESET}")
