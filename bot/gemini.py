c8d192e70bc2faeaa0239c4eb5621a52a4305a1e
ellettie <mi.5270gh@gmail.com>
2025-06-30 07:11:05 +0000

    geminiのchat_historyを利用するように変更

--- bot/gemini.py
+++ bot/gemini.py
@@ -13,6 +13,7 @@ MODEL = os.environ.get("MODEL", "gemini-2.5-flash")
 IMAGE_MODEL = os.environ.get("IMAGE_MODEL", "gemini-2.0-flash-preview-image-generation")
 
 client = genai.Client(api_key=GEMINI_API_KEY)
+chats = {}
 
 def save_image_to_temp(inline_data) -> str:
     """BytesIO から PIL で開いて、一時ファイルに保存。パスを返す。"""
@@ -25,7 +26,7 @@ def save_image_to_temp(inline_data) -> str:
         os.close(fd)
     return path
 
-def create_part_objs(parts: list[dict]) -> list[types.Content]:
+def create_part_objs(parts: list[dict]) -> types.Content:
     """
     parts = [
         {"file_data": {"mime_type": file mime type, "data": file byte data}},
@@ -42,19 +43,46 @@ def create_part_objs(parts: list[dict]) -> list[types.Content]:
                 data=part["file_data"]["data"]
             ))
     
-    contents = [
-        types.Content(
+    contents = types.Content(
             role="user",
             parts=part_objs,
         )
-    ]
     return contents
+
+def create_chat(parent_id: int | None = None, last_idx: int | None = None):
+    if parent_id is not None:
+        history = chats[parent_id].get_history()[:last_idx]
+    else:
+        history = None
+    grounding_tool = types.Tool(
+        google_search=types.GoogleSearch()
+    )
+    url_context_tool = types.Tool(
+        url_context=types.UrlContext()
+    )
+    generate_content_config = types.GenerateContentConfig(
+        tools=[grounding_tool, url_context_tool],
+        response_mime_type="text/plain",
+        system_instruction=[
+            types.Part.from_text(text="""あなたは優秀なAIアシスタントです。回答は指定がない限り日本語でしてください。
+                                 ユーザの質問は以下のように構造化されています。<**ユーザの名前**>: <質問内容>""")
+        ],
+    )
+    chat = client.aio.chats.create(
+        model=MODEL,
+        config=generate_content_config,
+        history=history
+    )
+    return chat
+
+def delete_chat(id: int):
+    del chats[id]
 
 async def generate_image(parts: list[dict]):
     contents = create_part_objs(parts)
     response = await client.aio.models.generate_content(
         model=IMAGE_MODEL,
-        contents=contents, # type: ignore
+        contents=contents,
         config=types.GenerateContentConfig(
             response_modalities=["TEXT", "IMAGE"],
         )
@@ -71,51 +99,24 @@ async def generate_image(parts: list[dict]):
             break
     return image_path, text, input_token, output_token
 
-async def generate_text(parts: list[dict], history=None):
+async def generate_text(
+        parts: list[dict], 
+        id: int, 
+        parent_id: int | None = None,
+        last_idx: int | None = None,
+        is_new_chat: bool = False
+        ):
     contents = create_part_objs(parts)
-    if history is not None:
-        for chat in history:
-            contents.insert(
-                0,
-                types.Content(
-                    role="model",
-                    parts=[
-                        types.Part.from_text(text=chat["ai"])
-                    ]
-                )
-            )
-            contents.insert(
-                0,
-                types.Content(
-                    role="user",
-                    parts=[
-                        types.Part.from_text(text=chat["user"])
-                    ]
-                )
-            )
-    grounding_tool = types.Tool(
-        google_search=types.GoogleSearch()
-    )
-    url_context_tool = types.Tool(
-        url_context=types.UrlContext()
-    )
-    generate_content_config = types.GenerateContentConfig(
-        tools=[grounding_tool, url_context_tool],
-        response_mime_type="text/plain",
-        system_instruction=[
-            types.Part.from_text(text="""あなたは優秀なAIアシスタントです。回答は指定がない限り日本語でしてください。
-                                 ユーザの質問は以下のように構造化されています。<**ユーザの名前**>: <質問内容>""")
-        ],
-    )
-    response = await client.aio.models.generate_content(
-        model=MODEL,
-        contents=contents, # type: ignore
-        config=generate_content_config,
+    if is_new_chat:
+        chats[id] = create_chat(parent_id, last_idx)
+    response = await chats[id].send_message(
+        message = contents.parts
     )
     text = add_citations(response)
     input_token = getattr(response.usage_metadata, "prompt_token_count", None)
     output_token = getattr(response.usage_metadata, "candidates_token_count", None)
-    return text or "エラーが発生しました", input_token, output_token
+    last_idx = len(chats[id].get_history())
+    return text or "エラーが発生しました", input_token, output_token, last_idx
 
 def add_citations(response) -> str:
     text = response.text.rstrip()
@@ -134,11 +135,7 @@ def add_citations(response) -> str:
 
     if not links:
         return text
-
-    # [1](url) をコンマ区切りで並べる
     footnotes = ", ".join(f"[{n}]({url})" for n, url in enumerate(links, 1))
-
-    # 好きな見出しに変えて OK
     separator = Config.RESPONSE_SEPARATOR
     return f"{text}{separator}{footnotes}"
 
@@ -150,5 +147,4 @@ def get_error_message(e: errors.APIError) -> str:
         desc = f"**{e.code}**\n\n{e.message}\nAPIサービスが混雑しています。時間を置いて再度試してください"
     else:
         desc = f"**{e.code}**\n\n{e.message}"
-        
     return desc
